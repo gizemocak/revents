237c22954bbe66f4c4711f202d199eb30e692ec1
Gizem Ocak <Gizem@Gizems-MacBook-Air.local>
2018-09-09 13:27:51 -0400

    End Of Section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import { Route, Switch } from "react-router-dom";
 import EventDashBoard from "../../features/event/EventDashBoard/EventDashBoard";
 import NavBar from "../../features/nav/NavBar/NavBar";
+import EventForm from "../../features/event/EventForm/EventForm";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashBoard";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import PoepleDashBoard from "../../features/user/PeopleDashBoard/PeopleDashBoard";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import HomePage from "../../features/home/HomePage";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <EventDashBoard />
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashBoard} />
+                  <Route path="/events/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PoepleDashBoard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
